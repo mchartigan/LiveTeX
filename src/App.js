814457d82bf77cbb9220010729601a460f50264e
mchartigan <hartigan@purdue.edu>
2020-12-21 21:47:11 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
+import MathJax from 'mathjax3-react';
+import { Container, Segment, Form, TextArea } from 'semantic-ui-react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: "",
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) { this.setState({ text: event.target.value }) }
+
+  parseText(text) {
+    // Replace newlines with LaTeX array newlines (\\)
+    let newText = text.replace(/(?:\n)/g, '\\\\');
+    // Put text in an array so multiple lines can be used
+    return ("$$\\begin{array}\\\\" + newText + "\\\\\\end{array}$$");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Container text>
+          <Segment>
+            <Form>
+              <TextArea
+                rows={5}
+                placeholder="Type LaTeX here"
+                value={this.state.text}
+                onChange={this.handleChange}
+              />
+            </Form>
+          </Segment>
+          <Segment>
+            <MathJax.Provider>
+              <MathJax.Formula formula={ this.parseText(this.state.text) } />
+            </MathJax.Provider>
+          </Segment>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
