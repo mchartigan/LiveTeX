277e6ab3f30c1ed3cfdd6c171b80b3ac323472fb
mchartigan <hartigan@purdue.edu>
2021-01-04 00:26:30 -0500

    Add logo and detailed explanation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import MathJax from 'mathjax3-react';
-import { Container, Segment, Form, TextArea, Icon, Image, Menu, Button, Header } from 'semantic-ui-react';
+import { Container, Segment, Form, TextArea, Icon, Image, Button, Accordion } from 'semantic-ui-react';
 import './App.css';
 
 // dynamically create lines to make multi-line equations
@@ -24,15 +24,24 @@ class App extends React.Component {
     this.state = {
       text: "",
       formulas: [],
+      activeIndex: 0,
     }
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   // update LaTeX in a controlled component
   handleChange(event) {
     this.setState({ text: event.target.value });
     this.setState({ formulas: event.target.value.split(/(?:\n)/g) });
+  }
+
+  handleClick(event, titleProps) {
+    const { index } = titleProps;
+    const newIndex = this.state.activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
   }
 
   render() {
@@ -44,11 +53,26 @@ class App extends React.Component {
               <Image src={process.env.PUBLIC_URL + '/favicon.ico'} avatar/>
               Go to Home
             </Button>
-            LiveTeX
+            <Image src={process.env.PUBLIC_URL + '/livetex.png'} centered/>
             <Button as="a" href="https://github.com/mchartigan/LiveTeX" floated="right">
               <Icon name="github"/> Visit on Github
             </Button>
           </Segment>
+          <Accordion fluid styled>
+            <Accordion.Title active={this.state.activeIndex === 0} index={0} onClick={this.handleClick}>
+              <Icon name='dropdown' />
+              Details
+            </Accordion.Title>
+            <Accordion.Content active={this.state.activeIndex === 0}>
+              <p>
+                Welcome to LiveTeX! This is an implementation of LaTeX's math mode, meant to be used to draft equations
+                or LaTeX equations for use in papers outside of LaTeX. It lacks the functionality of programs such as TeX Live or
+                sites like Overleaf, but in return offers live rendering and easy multi-line equation support.
+
+                Pending features include "Copy to Clipboard" and "Save" buttons, as well as shortcuts for often-used LaTeX commands.
+              </p>
+            </Accordion.Content>
+          </Accordion>
           <Segment>
             <Form>
               <TextArea
